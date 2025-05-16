61de53f8ce460d6a629c9c37a0e45d13961d7874
Chiara Facciolà <chiara.facciola@idems.international>
2025-05-16 13:15:33 +0200

    Update parenttext_models.py

--- models/parenttext_models.py
+++ models/parenttext_models.py
@@ -3,3 +3,10 @@ from rpft.parsers.common.rowparser import ParserModel
 from parenttext_pipeline.models.parenttext_models import *
 from typing import List
 
+
+class JoiningTriggerModel(DataRowModel):
+    triggers: str = ""
+    groups: list[str] = []
+    cap: str = ""
+    location: str = ""
+    set_location: str = "no"
